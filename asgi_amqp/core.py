db597da5cda20a072c5368e8e8041b1f56b5139f
Wayne Witzel III <wayne@riotousliving.com>
2016-09-16 09:43:22 -0400

    switch to kombu for amqp library

--- asgi_amqp/core.py
+++ asgi_amqp/core.py
@@ -1,73 +1,72 @@
 from __future__ import unicode_literals
 
 import pika
+import kombu
 import six
 import uuid
 import msgpack
-import logging
-import random
+import socket
 
 from asgiref.base_layer import BaseChannelLayer
-
-logging.getLogger('pika').propagate = False
+from collections import deque
 
 
 class AMQPChannelLayer(BaseChannelLayer):
     def __init__(self, url=None, prefix='asgi:', expiry=60, group_expiry=86400, capacity=100, channel_capacity=None):
         super(AMQPChannelLayer, self).__init__(expiry, capacity, group_expiry, channel_capacity)
         self.url = url or 'amqp://guest:guest@localhost:5672/%2F'
-        self.prefix = prefix
-        self.exchange = prefix + 'channels'
+        self.prefix = prefix + 'tower'
+        self.connection = pika.BlockingConnection(pika.connection.URLParameters(self.url))
+        self.exchange = kombu.Exchange(self.prefix, type='direct')
+        self._buffer = deque()
+
+        kombu.serialization.enable_insecure_serializers()
 
     def send(self, channel, message):
         assert isinstance(message, dict), "message is not a dict"
         assert self.valid_channel_name(channel), "Channel name not valid"
-
-        connection = self.connection()
-        sendchan = connection.channel()
-
-        sendchan.queue_declare(queue=channel, durable=False, exclusive=False)
-        sendchan.basic_publish(exchange='',
-                               routing_key=channel,
-                               body=self.serialize(message),
-                               properties=pika.BasicProperties(delivery_mode=1,content_type='text/plain'))
-        connection.close()
+        with kombu.Connection(self.url) as connection:
+            payload = self.serialize(message)
+            producer = kombu.Producer(connection, self.exchange, routing_key=channel)
+            producer.publish(payload, routing_key=channel, delivery_mode=1,
+                                content_type='application/msgkpack', content_encoding='binary')
+        print("SEND", channel)
 
     def receive_many(self, channels, block=False):
         if not channels:
             return None, None
 
-        connection = self.connection()
+        queue = kombu.Queue(exchange=self.exchange, exclusive=True,
+                            bindings=[kombu.binding(self.exchange, routing_key=channel)
+                                      for channel in channels])
 
-        channels = list(channels)
-        random.shuffle(channels)
-
-        for channel in channels:
-            try:
-                recvchan = connection.channel()
-                method_frame, header_frame, body = recvchan.basic_get(queue=channel)
-                if method_frame:
-                        recvchan.basic_ack(method_frame.delivery_tag)
-                        if '!' in channel:
-                            recvchan.queue_delete(channel)
-                        connection.close()
-                        return channel, self.deserialize(body)
-            except pika.exceptions.ChannelClosed:
-                continue
-
-        if block:
-            connection.sleep(0.1)
-        connection.close()
+        with kombu.Connection(self.url) as connection:
+            connection.default_channel.basic_qos(0, 1, False)
+            with kombu.Consumer(connection, queues=[queue], callbacks=[self.on_message]) as consumer:
+                consumer.qos(prefetch_count=1)
+                while True:
+                    print("CONSUMING...")
+                    if self._buffer:
+                        channel, message = self._buffer.popleft()
+                        message = self.deserialize(message)
+                        return channel, message
+                    try:
+                        connection.drain_events(timeout=1)
+                    except socket.timeout:
+                        continue
         return None, None
+
+    def on_message(self, body, message):
+        channel = message.delivery_info['routing_key']
+        print("MSG", channel)
+        self._buffer.append((channel, body))
+        message.ack()
 
     def new_channel(self, pattern):
         assert isinstance(pattern, six.text_type)
         assert pattern.endswith("!") or pattern.endswith("?")
         new_name = pattern + str(uuid.uuid4())
         return new_name
-
-    def connection(self):
-        return pika.BlockingConnection(pika.connection.URLParameters(self.url))
 
     def serialize(self, message):
         """
